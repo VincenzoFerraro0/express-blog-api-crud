d65aa3f161815937c9fd3056b173e6523c0d12b9
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-02-12 17:24:18 +0100

    add: express

---
+++ data/arrayPosts.js
@@ -0,0 +1,36 @@
+// array dei posts
+
+const posts = [
+    {
+        title: "Cracker alla barbabietola",
+        content: `I cracker alla barbabietola sono uno snack stuzzicante e originale...`,
+        image: "/img/cracker_barbabietola.jpeg",
+        tags: ["Antipasti", "Ricette vegetariane", "Ricette al forno"],
+    },
+    {
+        title: "Ciambellone soffice",
+        content: `Il ciambellone soffice è un dolce classico della tradizione...`,
+        image: "/img/ciambellone.jpeg",
+        tags: ["Dolci", "Colazione", "Ricette tradizionali"]
+    },
+    {
+        title: "Pane fritto dolce",
+        content: `Il pane fritto dolce è una ricetta semplice e golosa...`,
+        image: "/img/pane_fritto_dolce.jpeg",
+        tags: ["Dolci", "Merenda", "Ricette tradizionali"]
+    },
+    {
+        title: "Pasta alla barbabietola",
+        content: `La pasta alla barbabietola è un primo piatto colorato e dal sapore delicato...`,
+        image: "/img/pasta_barbabietola.jpeg",
+        tags: ["Primi piatti", "Ricette vegetariane", "Colorate e creative"]
+    },
+    {
+        title: "Torta paesana",
+        content: `La torta paesana è un dolce della tradizione lombarda...`,
+        image: "/img/torta_paesana.jpeg",
+        tags: ["Dolci", "Ricette tradizionali", "Riciclo in cucina"]
+    }
+];
+
+export default posts 
