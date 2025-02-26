1c5ed1ca4b320a7b0559f7d99c46260bdb83f26a
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-02-26 14:42:12 +0100

    add cors

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ import express from 'express'; // Importa il modulo Express
 import postsRouter from './routers/postsRouter.js'; // Importa il router per la gestione dei post
 import errorsHandler from './middlewares/errorsHandler.js';
 import notFound from './middlewares/notFound.js';
+import cors from 'cors'
 
 const app = express(); // Crea un'istanza dell'applicazione Express
 const port = 3000; // Definisce la porta su cui il server ascolterà
 
 // Middleware per gestire gli errori globali
 app.use(errorsHandler);
-
-
-
 
 // Configura Express per servire file statici dalla cartella 'public'
 app.use(express.static('public'));
@@ -26,7 +24,10 @@ app.get('/', (req, res) => {
 // Usa il router dei post per gestire le richieste relative ai post
 app.use("/api/posts", postsRouter);
 
-
+// middleware per il CORS
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 
 // Middleware per gestire le rotte non trovate (404)
 app.use(notFound);
